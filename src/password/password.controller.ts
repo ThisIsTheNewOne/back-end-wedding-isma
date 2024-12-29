29da05165e44e92c36fc52ad1604002257ba9b59
Paul Balta <baltapaul2@gmail.com>
2024-12-29 20:57:14 +0200

    adding the get password

--- src/password/password.controller.ts
+++ src/password/password.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('password')
-export class PasswordController {}
+export class PasswordController {
+    @Get('get-password')
+    getPassword(): { password: string } {
+      const basePassword = '12345';
+      const timestamp = Math.floor(Date.now() / 600000);  // Divide by 10 min (in ms)
+      const dynamicPassword = `${basePassword}-${timestamp}`;
+      return { password: dynamicPassword };
+    }
+  }
